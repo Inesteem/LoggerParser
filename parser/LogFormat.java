439eaabf4cfbd7b37ac23188239a0f7e9516a4de
Lucifer Hemp <lucifer.hemp@fau.de>
2020-09-17 17:09:01 +0200

    files added again

---
+++ parser/LogFormat.java
@@ -0,0 +1,24 @@
+package parser;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.util.Pair;
+
+public interface LogFormat{
+	
+	
+	public boolean get_values(String[] data, List<Double> values);
+	
+	
+	public Parser.ParserType get_parser_type();
+	
+	
+	public Pair<Integer, double[]> get_month_avg(Parser.Month m);
+	
+	
+	public String get_value_header();
+	
+}
---
+++ parser/LogInfo.java
@@ -0,0 +1,17 @@
+package parser;
+
+public class LogInfo {
+	
+	String line;
+	
+    public LogInfo(){
+		line = "";
+	} 
+	public void add_val(String line){
+		this.line = line;
+	}
+	
+	public String get_val(){
+		return this.line;
+	}
+}
